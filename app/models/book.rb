992c4a2288d96e1fa0c2f5c42182402316439d10
eric27n <73068837+eric27n@users.noreply.github.com>
2024-01-29 23:23:44 -0600

    book collection 2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,11 @@
 class Book < ApplicationRecord
+  validates :title, :author, :published_date, presence: true
+  validates :price, presence: true, numericality: true
+  validate :correct_date_format
+
+  private
+
+  def correct_date_format
+    errors.add(:published_date, 'must be a valid date in the format YYYY-MM-DD') unless published_date.is_a?(Date)
+  end
 end
